77036a5f9b3b1aaca86bda3f2de9bfc64787d66c
Sanush CJ <sanushcj@gmail.com>
2023-01-02 16:53:10 +0530

    mvc model done + navbar

---
+++ lib/controller/navbar_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class NavBarController extends GetxController{
+   var currentIndex = 2.obs;
+
+}
---
+++ lib/core/Colors/mycolors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const rrWhite = Colors.white;
+const rrBlack = Colors.black;
+const rrBlue = Colors.blue;
